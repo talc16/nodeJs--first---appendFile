f44ec3b0dc8ba24236bdfb5297eba727cf7bce9d
tal <talc16@gmail.com>
2019-10-30 11:13:36 +0200

    fix

--- start.js
+++ start.js
@@ -1,3 +1,3 @@
 const jsonFileAppend = require("./jsonAppend")
-const result = jsonFileAppend.appendToFile("./some.txt", "dsadddas")
+const result = jsonFileAppend.appendToFile("./info.txt", "Append new line")
 console.log(result)
